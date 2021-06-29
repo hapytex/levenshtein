5dacb8f37f104ae0ef102060d48d39b10fc60e87
Willem Van Onsem <vanonsem.willem@gmail.com>
2021-06-29 08:12:54 +0200

    extra bounds check

--- test/Data/Foldable/LevenshteinSpec.hs
+++ test/Data/Foldable/LevenshteinSpec.hs
@@ -16,16 +16,19 @@ spec = do
   it "if zero then same list" (quickCheckWith stdArgs { maxSuccess = 10000000 } (property (ifZeroThenSame @ Int)))
   it "test triangle inequality" (quickCheckWith stdArgs { maxSuccess = 10000000 } (property (triangleInequality @ Int)))
   it "test applying edits" (quickCheckWith stdArgs { maxSuccess = 10000000 } (property (testApplyingEdits @ Int)))
+  it "Hamming distance bound" (quickCheckWith stdArgs { maxSuccess = 10000000 } (property (hammingDistanceBound @ Int)))
   it "lowerbound string difference" (quickCheckWith stdArgs { maxSuccess = 10000000 } (property (lowerBoundLengthDiff @ Char)))
   it "upperbound largest string" (quickCheckWith stdArgs { maxSuccess = 10000000 } (property (upperBoundLengthDiff @ Char)))
   it "if zero then same list" (quickCheckWith stdArgs { maxSuccess = 10000000 } (property (ifZeroThenSame @ Char)))
-  it "test triangle inequality" (quickCheckWith stdArgs { maxSuccess = 10000000 } (property (triangleInequality @ Int)))
+  it "test triangle inequality" (quickCheckWith stdArgs { maxSuccess = 10000000 } (property (triangleInequality @ Char)))
   it "test applying edits" (quickCheckWith stdArgs { maxSuccess = 10000000 } (property (testApplyingEdits @ Char)))
+  it "Hamming distance bound" (quickCheckWith stdArgs { maxSuccess = 10000000 } (property (hammingDistanceBound @ Char)))
   it "lowerbound string difference" (quickCheckWith stdArgs { maxSuccess = 10000000 } (property (lowerBoundLengthDiff @ Bool)))
   it "upperbound largest string" (quickCheckWith stdArgs { maxSuccess = 10000000 } (property (upperBoundLengthDiff @ Bool)))
   it "if zero then same list" (quickCheckWith stdArgs { maxSuccess = 10000000 } (property (ifZeroThenSame @ Bool)))
   it "test triangle inequality" (quickCheckWith stdArgs { maxSuccess = 10000000 } (property (triangleInequality @ Bool)))
   it "test applying edits" (quickCheckWith stdArgs { maxSuccess = 10000000 } (property (testApplyingEdits @ Bool)))
+  it "Hamming distance bound" (quickCheckWith stdArgs { maxSuccess = 10000000 } (property (hammingDistanceBound @ Bool)))
 
 
 lowerBoundLengthDiff :: forall a . Eq a => [a] -> [a] -> Bool
@@ -42,3 +45,12 @@ ifZeroThenSame xs ys = (levenshteinDistance xs ys == (0 :: Int)) == (xs == ys)
 
 testApplyingEdits :: forall a . Eq a => [a] -> [a] -> Bool
 testApplyingEdits xs ys = applyEdits (snd (levenshtein xs ys)) xs == Just ys
+
+hammingDistanceBound :: forall a . Eq a => [a] -> [a] -> Bool
+hammingDistanceBound xs ys = length xs /= length ys || levenshteinDistance xs ys <= hammingDistance xs ys
+
+hammingDistance :: forall a . Eq a => [a] -> [a] -> Int
+hammingDistance (x:xs) (y:ys)
+  | x == y = 1 + hammingDistance xs ys
+  | otherwise = hammingDistance xs ys
+hammingDistance _ _ = 0
