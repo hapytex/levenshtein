f3d95bd1f28d29f6f60775a368a0b275a2757a21
Willem Van Onsem <vanonsem.willem@gmail.com>
2021-07-08 20:43:11 +0200

    add benchmarking to the tests

--- bench/levenshtein-bench.hs
+++ bench/levenshtein-bench.hs
@@ -35,17 +35,31 @@ main = do
     [ bgroup "same"
       [ bgroup "small"
         [ bench "lib" $ (nf levInt) (smallA, smallA)
+        , bench "lib" $ (nf levInt) (smallB, smallB)
         ]
       , bgroup "big"
         [ bench "lib" $ (nf levInt) (bigA, bigA)
+        , bench "lib" $ (nf levInt) (bigB, bigB)
         ]
       ]
     , bgroup "different"
       [ bgroup "small"
         [ bench "lib" $ (nf levInt) (smallA, smallB)
+        , bench "lib" $ (nf levInt) (smallB, smallA)
+        ]
+      , bgroup "mixed"
+        [ bench "lib" $ (nf levInt) (smallA, bigB)
+        , bench "lib" $ (nf levInt) (bigB, smallA)
+        , bench "lib" $ (nf levInt) (smallA, bigB)
+        , bench "lib" $ (nf levInt) (bigB, smallA)
+        , bench "lib" $ (nf levInt) (smallB, bigB)
+        , bench "lib" $ (nf levInt) (bigB, smallB)
+        , bench "lib" $ (nf levInt) (smallB, bigA)
+        , bench "lib" $ (nf levInt) (bigA, smallB)
         ]
       , bgroup "big"
         [ bench "lib" $ (nf levInt) (bigA, bigB)
+        , bench "lib" $ (nf levInt) (bigB, bigA)
         ]
       ]
     ]
